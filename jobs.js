c81a2d1b29f2affa027b5a96a63d97b4e930b71d
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-06-25 18:04:50 +0500

    adding api

--- jobs.js
+++ jobs.js
@@ -1,21 +1,18 @@
-const jobtitle = document.querySelectorAll(".job-title-front");
-console.log(jobtitle);
+// const jobtitle = document.querySelectorAll(".job-title-front");
+// console.log(jobtitle);
 
 fetch("https://6674243575872d0e0a95448f.mockapi.io/v1/mkjg")
   .then((response) => {
     return response.json();
   })
   .then((names) => {
-    // var values = names.[9
-    // console.log("names", names[0]);
     names.forEach((element) => {
-      var uses = element;
-      //   console.log(element);
-      //   return uses;
-      const text = document.createTextNode(jobtitle);
-      console.log(text);
+      const markup = `<div>${element}</div>`;
+      const windowObj = window.document.querySelectorAll(
+        ".job-thumbnail-front"
+      );
+      console.log(windowObj[0].insertAdjacentHTML("beforeend", markup));
     });
   })
 
-  .catch();
-// console.log(names);
+  .catch((error) => console.log(error));
